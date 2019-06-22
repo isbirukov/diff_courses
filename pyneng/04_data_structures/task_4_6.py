4a0df703ec22c5681d2fea6ea5e15059584f1a5b
biryukovis <isbirukov@gmail.com>
2019-06-22 18:46:12 +0400

    task 4.6

--- pyneng/04_data_structures/task_4_6.py
+++ pyneng/04_data_structures/task_4_6.py
@@ -15,3 +15,15 @@ Outbound Interface     FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = ospf_route.replace(',', '')
+ospf_route = ospf_route.strip().split()
+ospf_route[2] = ospf_route[2].strip('[]')
+ospf_route.pop(0)
+ospf_route.pop(2)
+
+print("""Protocol:{:>18}
+Prefix:{:>28}
+AD/Metric:{:>19}
+Next-Hop:{:>23}
+Last update:{:>16}
+Outbound Interface{:>20}""".format('OSPF', ospf_route[0], ospf_route[1], ospf_route[2], ospf_route[3], ospf_route[4]))
